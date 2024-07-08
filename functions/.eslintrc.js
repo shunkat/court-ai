5fa7398277fad8b368bbb18a2a916c64261887fe
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-07-09 02:03:38 +0900

    chore: lint style

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,33 +1,35 @@
 module.exports = {
-  root: true,
-  env: {
+  'root': true,
+  'env': {
     es6: true,
     node: true,
   },
-  extends: [
+  'extends': [
     'eslint:recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
     'google',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
     project: ['tsconfig.json', 'tsconfig.dev.json'],
     sourceType: 'module',
   },
-  ignorePatterns: [
+  'ignorePatterns': [
     '/lib/**/*', // Ignore built files.
     '/generated/**/*', // Ignore generated files.
   ],
-  plugins: ['@typescript-eslint', 'import'],
-  rules: {
-    'quotes': ['error', 'single'],
+  'plugins': ['@typescript-eslint', 'import'],
+  'rules': {
     'object-curly-spacing': ['error', 'always'],
     'import/no-unresolved': 0,
     'indent': ['error', 2],
+    'valid-jsdoc': 'off',
     'max-len': ['error', { 'code': 200 }],
+    'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
     '@typescript-eslint/no-unused-vars': ['error', {
       'argsIgnorePattern': '^_',
       'varsIgnorePattern': '^_',
