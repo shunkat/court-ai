061e676b4ed2b2d3d6dd712cda61b1bccbb68b48
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-07-15 22:59:27 +0900

    refactor: limit指定

--- functions/src/firestore/chat.ts
+++ functions/src/firestore/chat.ts
@@ -6,30 +6,17 @@ export const addChat = async (chat: ChatSchema): Promise<void> => {
   await db.collection(`chats`).add({ ...chat, createdAt: FieldValue.serverTimestamp() });
 };
 
-export const getChatsFromRoom = async (roomId: string): Promise<ChatSchema[]> => {
-  const snapshot = await db.collection('chats')
-    .where('roomId', '==', roomId)
-    .orderBy('createdAt', 'asc')
-    .get();
+export const getChatsFromRoomUser = async (roomUserId: string, option?: {
+  last?: number
+}): Promise<ChatSchema[]> => {
+  let q = db.collection('chats').where('roomUserId', '==', roomUserId);
 
-  const chats = snapshot.docs.map((doc) => {
-    const chat = chatSchema.safeParse(doc.data());
-    if (chat.success) return chat.data;
-    else {
-      console.error('Failed to getChats\nInvalid chat data:', chat.error.errors);
-      return;
-    }
-  }).filter((c) => !!c);
+  if (option?.last) {
+    q = q.limit(option.last) .orderBy('createdAt', 'desc');
+  }
 
-  return chats;
-};
+  const snapshot = await q.get();
 
-export const getChatsFromUser = async (roomUserId: string, last = 5): Promise<ChatSchema[]> => {
-  const snapshot = await db.collection('chats')
-    .where('roomUserId', '==', roomUserId)
-    .orderBy('createdAt', 'desc')
-    .limit(last)
-    .get();
 
   const chats = snapshot.docs.map((doc) => {
     const chat = chatSchema.safeParse(doc.data());
