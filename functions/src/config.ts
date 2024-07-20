ab8438a83398c308e486cc1c415d4ceadd73c5dd
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-07-20 19:09:34 +0900

    chore: Update global options in config.ts

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,15 +1,14 @@
 import { initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import { defineSecret } from 'firebase-functions/params';
 import { setGlobalOptions } from 'firebase-functions/v2/options';
 import { configureGenkit } from '@genkit-ai/core';
 import { firebase } from '@genkit-ai/firebase';
 import googleAI from '@genkit-ai/googleai';
+import { googleGenaiApiKey, resendApiKey } from './env';
 
-const googleGenaiApiKey = defineSecret('GOOGLE_GENAI_API_KEY');
 setGlobalOptions({
   region: 'us-west2',
-  secrets: [googleGenaiApiKey],
+  secrets: [googleGenaiApiKey, resendApiKey],
   timeoutSeconds: 540,
   maxInstances: 5,
 });
