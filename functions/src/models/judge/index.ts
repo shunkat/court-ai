4f427d8b46d7266cd37ee9c7efe6120a123b0fcb
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-07-28 16:32:26 +0900

    feat: Add summary functionality to Firestore

--- functions/src/models/judge/index.ts
+++ functions/src/models/judge/index.ts
@@ -2,9 +2,10 @@ import { generate } from '@genkit-ai/ai';
 import { MessageData } from '@genkit-ai/ai/model';
 import { geminiPro } from '@genkit-ai/googleai';
 import { z } from 'zod';
-import { judgeSystemPrompt } from './prompt';
+import { getSummarizePrompt, judgeSystemPrompt } from './prompt';
 import { defineFlow } from '@genkit-ai/flow';
 import { modelRoleSchema } from '../lawyer/schema';
+import { summarizeOutputSchema } from './schema';
 
 export const judgeSuggestionFlow = defineFlow(
   {
@@ -32,5 +33,34 @@ export const judgeSuggestionFlow = defineFlow(
     });
 
     return llmResponse.text();
+  },
+);
+
+export const judgeSummarizeFlow = defineFlow(
+  {
+    name: 'judgeSummarizeFlow',
+    inputSchema: z.object({
+      conversation: z.string(),
+      history: z.array(z.object({
+        role: modelRoleSchema,
+        content: z.array(z.object({ text: z.string() })),
+      })),
+    }),
+    outputSchema: summarizeOutputSchema.nullable(),
+  },
+  async (input) => {
+    const history: MessageData[] = [
+      { role: 'system', content: [{ text: judgeSystemPrompt }] },
+      ...input.history,
+    ];
+
+    const llmResponse = await generate({
+      model: geminiPro,
+      prompt: getSummarizePrompt(input.conversation),
+      history,
+      config: { temperature: 1 },
+    });
+
+    return llmResponse.output();
   },
 );
